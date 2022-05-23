6abec1220445130536c4bfb8b5f196af05da0c5e
fatamatuzjaharasamiha <samirajahara843@gmail.com>
2022-05-24 05:06:44 +0600

    create folder structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './Pages/Shared/Navbar';
+
 
 function App() {
   return (
     <div className="App">
+     <Navbar></Navbar>
      
     </div>
   );
