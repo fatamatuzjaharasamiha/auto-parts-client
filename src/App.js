0e07594aca459ab952afe06955c85f6df3795a7a
fatamatuzjaharasamiha <samirajahara843@gmail.com>
2022-05-24 11:34:55 +0600

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Blogs from './Pages/Blogs/Blogs';
+import Home from './Pages/Home/Home';
 import Navbar from './Pages/Shared/Navbar';
 
 
 function App() {
   return (
     <div className="App">
-     <Navbar></Navbar>
-     
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>} />
+        <Route path="/blogs" element={<Blogs></Blogs>} />
+        
+      </Routes>
     </div>
   );
 }
